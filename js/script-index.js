5a313803e62fa93227f23ba91552aad24b01150f
mickequeen <mickequeen@gmail.com>
2018-01-08 12:01:10 -0300

    etapa 3: creando función renderHighlightedRecipes

--- js/script-index.js
+++ js/script-index.js
@@ -17,7 +17,11 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+	for(var i= 0; i<recipesArray.length;i++){
+    if(recipesArray[i].highlighted==true){
+      renderRecipe(recipesArray[i]);
+      }
+}
 }
 
 /*
